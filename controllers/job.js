a7b932757b072a44fc3fe5d893864ad48283c4de
Avinash kumar <ak2783934@gmail.com>
2022-07-06 17:55:26 +0530

    everybody will recieve emails now

--- controllers/job.js
+++ controllers/job.js
@@ -1,5 +1,28 @@
 const Job = require("../models/Job");
 
+const FreeJobAlerts = require("../models/FreeJobAlert");
+const { sendCustomEmail } = require("../Util/sendEmail");
+
+const sendJobNotificationToEveryOne = (job) => {
+  //what we have to do is to send the notification to everyone who are in our free job alert list
+  FreeJobAlerts.find().exec((err, freeJobAlerts) => {
+    if (err || !freeJobAlerts) {
+      console.log("No free alert subscribed");
+    }
+    const userList = [];
+    freeJobAlerts.map((freeJobAlert) => {
+      const user = {
+        name: freeJobAlert.name,
+        email: freeJobAlert.emailId,
+      };
+      userList.push(user);
+    });
+    const subject = "NEW JOB AVAILABLE";
+    const htmlContent = `<html><body><h2>New job alert</h2><p>Hey job seeker, There is a new opening at mscjobs.in, please visit to this link to know more <a href="https://www.mscjobs.in/jobsearch?searchVal=${job.companyName}">LINK</a></p></body></html>`;
+    sendCustomEmail(userList, subject, htmlContent);
+  });
+};
+
 exports.postJob = (req, res) => {
   console.log(req.body);
   console.log(req.file);
@@ -12,6 +35,7 @@ exports.postJob = (req, res) => {
         error: "NOT able to save job in DB",
       });
     }
+    sendJobNotificationToEveryOne(job);
     res.json({ job });
   });
 };
