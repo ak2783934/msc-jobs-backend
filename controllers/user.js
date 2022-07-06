a9cf735b23f0b7bb25d8ccecf2dcf0a0e020777d
Avinash kumar <ak2783934@gmail.com>
2022-07-06 14:49:48 +0530

    user routes updated

--- controllers/user.js
+++ controllers/user.js
@@ -18,3 +18,25 @@ exports.getUser = (req, res) => {
   req.profile.password = undefined;
   return res.json(req.profile);
 };
+
+exports.validateNewUser = (req, res) => {
+  const reqId = req.body.reqUserId;
+
+  User.findOneAndUpdate({ _id: reqId }, { isVerified: true }).exec(
+    (err, user) => {
+      if (err || !user) {
+        return res.status(400).json({ error: "No user updated" });
+      }
+      return res.send({ user });
+    }
+  );
+};
+
+exports.getAllUnverifiedUsers = (req, res) => {
+  User.find({ isVerified: false }).exec((err, users) => {
+    if (err || !users) {
+      return res.status(400).json({ error: "No users unverified" });
+    }
+    return res.send({ users });
+  });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser } = require("../controllers/user");
+const {
+  getUserById,
+  getUser,
+  validateNewUser,
+  getAllUnverifiedUsers,
+} = require("../controllers/user");
 
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
 router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+
+router.post(
+  "/user/verify/:userId",
+  isSignedIn,
+  isAuthenticated,
+  validateNewUser
+);
+
+router.get(
+  "/user/getAllUnverified/:userId",
+  isSignedIn,
+  isAuthenticated,
+  getAllUnverifiedUsers
+);
 
 module.exports = router;
